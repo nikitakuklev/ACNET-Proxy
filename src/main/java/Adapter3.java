2bb618a6b729f16516b5d237347f55b140330f36
nikitakuklev <spontaneouscoder@gmail.com>
2023-11-14 18:35:00 -0600

    Initialize only specific device classes

--- src/main/java/Adapter3.java
+++ src/main/java/Adapter3.java
@@ -1,10 +1,10 @@
 import gov.fnal.controls.daq.acquire.SettingsState;
 import gov.fnal.controls.kerberos.KerberosLoginContext;
 import gov.fnal.controls.service.dmq.*;
-//import gov.fnal.controls.service.dmq.impl2.probe.SettingApp;
 import gov.fnal.controls.tools.dio.DIODMQ;
 import gov.fnal.controls.tools.drf2.DataRequest;
 import gov.fnal.controls.tools.drf2.DiscreteRequest;
+import gov.fnal.controls.tools.logging.LogFormatter;
 import gov.fnal.controls.tools.logging.LogInit;
 import gov.fnal.controls.tools.timed.TimedError;
 import gov.fnal.controls.tools.timed.TimedNumber;
@@ -13,29 +13,27 @@ import io.undertow.UndertowOptions;
 import io.undertow.server.HttpHandler;
 import org.xnio.Options;
 
+import javax.security.auth.Subject;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 import java.nio.charset.Charset;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
-
-
 
 public class Adapter3 {
 
-    private static final Logger logger = Logger.getLogger(Adapter3.class.getName());
+    private static Logger logger =null;
     public static DaqClient daqReadingClient = null;
     public static DaqClient daqSettingClient = null;
-
     public static LinkedList<DaqClient> daqSettingClients = null;
     public static DaqClient daqDefaultClient = null;
     //public static SettingJob settingJob = null;
@@ -48,14 +46,16 @@ public class Adapter3 {
     public static HashSet<String> setters = null;
     public static LinkedList<HashSet<String>> settersList = new LinkedList<>();
     public static String preset;
-    public static DefaultCredentialHandler credHandler = null;
+    public static CredHandler credHandler = null;
+    public static Subject defaultCreds = null;
 
     public static void main(String[] args) {
         //System.setProperty("dmq.heartbeat-rate", "1000");
         //System.setProperty("dmq.amqp-heartbeat-rate", "1000");
-        System.setProperty("dmq.max-idle-time", "0"); //15000
-        System.setProperty("dmq.client.init-rate", "2000"); //5000
-        System.setProperty("dmq.client.lock-timeout", "4000"); //8000
+        //System.setProperty("dmq.max-idle-time", "0"); //15000
+        //System.setProperty("dmq.client.init-rate", "2000"); //5000
+        //System.setProperty("dmq.client.lock-timeout", "4000"); //8000
+        System.setProperty("dmq.client.purge-rate", "600000");
 
         settingJobs = new LinkedList<>();
         settingCallbackMap = new ConcurrentHashMap<>();
@@ -68,6 +68,31 @@ public class Adapter3 {
         }
 
         LogInit.initializeLogs();
+        Handler[] var0 = Logger.getLogger("").getHandlers();
+        for (Handler var3 : var0) {
+            if (var3 instanceof FileHandler) {
+                Logger.getLogger("").removeHandler(var3);
+            }
+        }
+        logger = Logger.getLogger(Adapter3.class.getName());
+
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
+        String fname = String.format("proxy.%s", timeStamp) + ".%u.%g.log";
+        try {
+            FileHandler hh = new FileHandler(fname, 20000000, 10, true);
+            hh.setLevel(Level.ALL);
+            hh.setFormatter(new LogFormatter());
+            Logger.getLogger("").addHandler(hh);
+            logger.config(String.format("File logging handler added with pattern [%s]",fname));
+        } catch (IOException ex) {
+            logger.log(Level.WARNING, "Unable to create logging FileHandler", ex);
+        }
+
+
+        Handler[] handlers = Logger.getLogger("").getHandlers();
+        // Log handler length
+        logger.info("Logging handlers: "  + handlers.length);
+
         logger.setUseParentHandlers(true);
         logger.log(Level.INFO, "ACNET-Proxy starting up with preset " + preset);
         logger.info("TEMP: " + System.getProperty("java.io.tmpdir"));
@@ -102,40 +127,49 @@ public class Adapter3 {
 //            settersList.add(setters_temp);
 //            logger.info(String.format("Setters list of length %d read", setters_temp.size()));
 
-//            N:IO01BI.SETTING
-//            N:IO02BI.SETTING
-//            N:IO03BI.SETTING
-//            N:IO04BI.SETTING
-//            N:IO05BI.SETTING
-//            N:IO06BI.SETTING
-//            N:IO07BI.SETTING
-//            N:IO08BI.SETTING
-//            N:IO09BI.SETTING
+
+
+//N:OB9L6I.SETTING
+//N:O4L6I.SETTING
+//N:O14L6I.SETTING
 //Z:ACLTST.SETTING
+
+//          N:IRFEAT.SETTING
 
             logger.info(">> DMQ config names: " + ConfigurationLoader.getConfigNames());
             Thread.sleep(500);
             logger.info(">> Authenticating");
-            credHandler = new DefaultCredentialHandler();
+            KerberosLoginContext krb = KerberosLoginContext.getInstance();
+            Class<? extends KerberosLoginContext> obj1 = krb.getClass();
+            Field field1 = obj1.getDeclaredField("loggedIn");
+            field1.setAccessible(true);
+            boolean loggedIn = (boolean)field1.get(krb);
+            if (!loggedIn) {
+                System.out.println( "KRB not logged in, trying to auth..." );
+                krb.login();
+            }
+            logger.info("Logged in as " + krb.getSubject().getPrincipals());
+
+            credHandler = new CredHandler(krb.getSubject());
             // Use reflection
-            Class<? extends DefaultCredentialHandler> obj = credHandler.getClass();
-            Field field = obj.getDeclaredField("krb5");
-            field.setAccessible(true);
-            KerberosLoginContext krb5 = (KerberosLoginContext)field.get(credHandler);
-            krb5.login();
-            logger.info(String.format(">> Authenticated as %s", krb5.getTicket().getClient()));
+            //Class<? extends DefaultCredentialHandler> obj = credHandler.getClass();
+            //Field field = obj.getDeclaredField("krb5");
+            //field.setAccessible(true);
+            //KerberosLoginContext krb5 = (KerberosLoginContext)field.get(credHandler);
+            //krb5.login();
+            //logger.info(String.format(">> Authenticated as %s", krb5.getTicket().getClient()));
 
             daqDefaultClient = new DaqClient();
-            //daqDefaultClient.enableSetting(Integer.MAX_VALUE, TimeUnit.SECONDS);
             daqDefaultClient.setCredentialHandler(credHandler);
             DIODMQ.setDaqClient(daqDefaultClient);
             selfTest();
             DIODMQ.enableSettings(true, SettingsState.FOREVER);
 
+            Subject creds = daqDefaultClient.getCredentials();
+            defaultCreds = creds;
+
             DRFCache.CACHE = new ConcurrentHashMap<>(1024);
             DRFCache.NAMEMAP = new ConcurrentHashMap<>(1024);
-
-            Thread.sleep(500);
 
             logger.info("Setting up default devices");
             ClassLoader classLoader = Adapter3.class.getClassLoader();
@@ -162,10 +196,18 @@ public class Adapter3 {
             //startReadingDaqJob(s2, "bpm_r", 5000);
 
             //setters = new HashSet<>();
-            int cnt = 0;
-            List<String> slist = Stream.of("sdevices1.txt", "sdevices2.txt",
-                    "sdevices3.txt", "sdevices4.txt", "sdevices5.txt").collect(Collectors.toList());
-            //, N:LGINJ.SETTING
+            AtomicInteger cnt = new AtomicInteger();
+            List<String> slist = Stream.of(
+                    "rf.txt",
+                    "quads.txt",
+                    "d_coils.txt",
+                    "dtrims.txt",
+                    "nlmags.txt",
+                    "octupoles.txt",
+                    "kickerdbl.txt",
+                    //"kickerctrl.txt",
+                    "sextupoles.txt"
+                    ).collect(Collectors.toList());
             for (String sfile : slist) {
                 BufferedReader is3 = new BufferedReader(new InputStreamReader(classLoader.getResourceAsStream(sfile)));
                 List<String> s3set = is3.lines().map(s -> s.split("@")[0]).collect(Collectors.toList());
@@ -180,13 +222,49 @@ public class Adapter3 {
                     String device_name = req_parsed.getDevice();
                     DRFCache.NAMEMAP.put(device_name, device);
                 });
-
-                //Thread.sleep(500);
-                //startReadingDaqJob(s3, String.format("Reader %d", cnt), 200000);
+                startReadingDaqJob(s3, String.format("Reader %d", cnt.intValue()), 200000);
                 Thread.sleep(500);
-                startSettingDaqJob(s3set);
-                cnt += 1;
+                startSettingDaqJob(s3set, creds);
+                cnt.addAndGet(1);
             }
+
+//            List<String> slist2 = Stream.of(
+//                    "kickerctrl.txt"
+//                    ).collect(Collectors.toList());
+//            for (String sfile : slist2) {
+//                BufferedReader is3 = new BufferedReader(new InputStreamReader(classLoader.getResourceAsStream(sfile)));
+//                List<String> s3set = is3.lines().map(s -> s.split("@")[0]).collect(Collectors.toList());
+//                for (String dev: s3set) {
+//                    DiscreteRequest req_parsed = (DiscreteRequest) DataRequest.parse(dev);
+//                    String device_name = req_parsed.getDevice();
+//                    logger.info(String.format("Creating individual job+client for [%s] (drf: %s)", dev, device_name));
+//                    UndertowHandler.getSettingJob(dev); //+ "@N"
+//                }
+//            }
+
+//            List<String> slist2 = Stream.of("d_coils.txt").collect(Collectors.toList());
+//            for (String sfile : slist2) {
+//                BufferedReader is3 = new BufferedReader(new InputStreamReader(classLoader.getResourceAsStream(sfile)));
+//                List<String> s3set = is3.lines().map(s -> s.split("@")[0]).collect(Collectors.toList());
+//                //List<String> s3 = is3.lines().map(s -> s.split("@")[0] + "@p,5000").collect(Collectors.toList());
+//                Collections.sort(s3set);
+//
+//                Utils.batches(s3set,4).forEach( dl -> {
+//                    HashSet<String> setters_temp = new HashSet<>(s3set);
+//                    settersList.add(setters_temp);
+//                    logger.info(String.format("Setters list (%s) of length (%d) read", sfile, setters_temp.size()));
+//                    Stream.of(setters_temp).flatMap(Collection::stream).forEach(device -> {
+//                        DiscreteRequest req_parsed = (DiscreteRequest) DataRequest.parse(device);
+//                        String device_name = req_parsed.getDevice();
+//                        DRFCache.NAMEMAP.put(device_name, device);
+//                    });
+//                    //Thread.sleep(50);
+//                    //startReadingDaqJob(s3, String.format("Reader %d", cnt), 200000);
+//                    //Thread.sleep(50);
+//                    startSettingDaqJob(s3set);
+//                    cnt.addAndGet(1);
+//                });
+//            }
 
 //            List<String> slist2 = Stream.of( "sdevices6.txt").collect(Collectors.toList());
 //            for (String sfile : slist2) {
@@ -233,14 +311,13 @@ public class Adapter3 {
             TimedNumber temp = DIODMQ.readDevice("M:OUTTMP@I");
             Thread.sleep(500);
             logger.info("M:OUTTMP@I = " + temp + " | time: " + DIODMQ.computedTime);
+            Thread.sleep(500);
             TimedNumber hum = DIODMQ.readDevice("M:OUTHUM.READING@I");
             logger.info("M:OUTHUM.READING@I = " + hum + " | time: " + DIODMQ.computedTime);
             logger.info(String.format("It is %f %s outside and humidity is %f %s",
                     temp.doubleValue(), temp.getUnit(), hum.doubleValue(), hum.getUnit()));
             logger.info("Read test - OK");
-
             Thread.sleep(500);
-
             logger.info("Write test");
             TimedError status = DIODMQ.setDevice("Z_ACLTST", 5.5);
             logger.info("Z_ACLTST setting status = " + status + " | time: " + DIODMQ.computedTime);
@@ -298,14 +375,18 @@ public class Adapter3 {
         logger.info("Authorized as:" + daqReadingClient);
     }
 
-    private static void startSettingDaqJob(List<String> devices) {
+    public static SettingJob startSettingDaqJob(List<String> devices, Subject creds) throws InterruptedException {
         //if (daqSettingClient == null) {
-        daqSettingClient = new DaqClient(preset);
-        daqSettingClient.setCredentialHandler(credHandler);
-        daqSettingClient.enableSetting(SettingsState.FOREVER, TimeUnit.MINUTES);
+        DaqClient dcl = new DaqClient(preset);
+        credHandler.last_cred_time = 0;
+        dcl.setCredentialHandler(credHandler);
+        //dcl.setCredentials(creds);
+        Thread.sleep(200);
+        //dcl.enableSetting(SettingsState.FOREVER, TimeUnit.MINUTES);
+        Thread.sleep(200);
         //}
-        daqSettingClients.add(daqSettingClient);
-        SettingJob sj = daqSettingClient.createSettingJob(new HashSet<>(devices));
+        daqSettingClients.add(dcl);
+        SettingJob sj = dcl.createSettingJob(new HashSet<>(devices));
         SettingCallback cb = new SettingCallback();
         sj.addDataCallback(cb);
         settingJobs.add(sj);
@@ -315,14 +396,16 @@ public class Adapter3 {
         //DaqClient dc = new DaqClient();
         //dc.setCredentialHandler(new DefaultCredentialHandler());
         //DIODMQ.setDaqClient(dc);
-        //DIODMQ.enableSettings(true, SettingsState.FOREVER);
-        logger.info(String.format("Client created for %d devices, settings: %s", devices.size(),daqSettingClient.isSettingEnabled()));
+        DIODMQ.enableSettings(true, SettingsState.FOREVER);
+        logger.info(String.format("Client created for %d devices, settings: %s", devices.size(),dcl.isSettingEnabled()));
+        dcl.enableSetting(SettingsState.FOREVER, TimeUnit.MINUTES);
+        return sj;
     }
 
     private static void setupUndertowRelay() {
         Undertow.Builder server = Undertow.builder();
         // avoid concurrency issues
-        server.setIoThreads(1);
+        server.setIoThreads(2);
         server.setWorkerThreads(1);
         server.setDirectBuffers(true);
         server.setBufferSize(1024 * 64 - 20);
@@ -349,4 +432,15 @@ public class Adapter3 {
     }
 }
 
-
+class Utils {
+    public static <T> Stream<List<T>> batches(List<T> source, int length) {
+        if (length <= 0)
+            throw new IllegalArgumentException("length = " + length);
+        int size = source.size();
+        if (size <= 0)
+            return Stream.empty();
+        int fullChunks = (size - 1) / length;
+        return IntStream.range(0, fullChunks + 1).mapToObj(
+                n -> source.subList(n * length, n == fullChunks ? size : (n + 1) * length));
+    }
+}
